474b5cb7d2b4e0e0a1279be78344cd1b99d86ede
logickoder <chukwudumebiorazulike@gmail.com>
2023-02-27 07:51:57 +0100

    feat: finished the file metadata application

---
+++ src/main/kotlin/dev/logickoder/filemetadata/FileMetadata.kt
@@ -0,0 +1,7 @@
+package dev.logickoder.filemetadata
+
+data class FileMetadata(
+    val name: String?,
+    val type: String?,
+    val size: Long?
+)
--- src/main/kotlin/dev/logickoder/filemetadata/FileMetadataController.kt
+++ src/main/kotlin/dev/logickoder/filemetadata/FileMetadataController.kt
@@ -1,12 +1,24 @@
 package dev.logickoder.filemetadata
 
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.util.concurrent.atomic.AtomicLong
+import org.springframework.web.multipart.MultipartFile
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
 
 @RestController
 object FileMetadataController {
 
+    @PostMapping("/api/fileanalyse")
+    fun analyzeFile(
+            @RequestParam("upfile") file: MultipartFile,
+            redirectAttributes: RedirectAttributes
+    ): FileMetadata {
+        return FileMetadata(
+            name = file.originalFilename,
+            type = file.contentType,
+            size = file.size,
+        )
+    }
 }
